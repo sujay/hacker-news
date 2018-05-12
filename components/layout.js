e00ad955407440ac126e002686d0715a076f9e89
Sujay <me@isujay.com>
2018-05-12 16:18:09 -0500

    split css into scoped and global styles

--- components/layout.js
+++ components/layout.js
@@ -25,20 +25,10 @@ const Layout = (props) => (
         Built in <a href="https://reactjs.org/">React</a> &amp; <a href="https://nextjs.org/">Next</a>.<br />
         HN API from <a href="https://github.com/cheeaun/node-hnapi/">node-hnapi</a>.</footer>
     </div>
-
-    <style jsx global>{`
-      body {
-        font-family: system-ui, -apple-system, BlinkMacSystemFont, "Helvetica Neue", Helvetica, Arial, sans-serif;
-        line-height: 1.15em;
-        color: #333;
-      }
+    <style jsx>{`
       header {
         background-color: #F96630;
         padding: 20px;
-      }
-      h1,h2,h3,h4,h5,h6 {
-        font-weight: 500;
-        line-height: 1.2;
       }
       h1 {
         font-size: 40px;
@@ -53,6 +43,35 @@ const Layout = (props) => (
         color: #FFF;
         text-decoration: none;
       }
+      #main {
+        background-color: #FFF;
+      }
+      footer {
+        font-size: 13px;
+        color: #bbb;
+        padding: 30px 0;
+        text-align: center;
+      }
+      footer a {
+        color: #FFF;
+      }
+      footer a:hover {
+        color: #FFF;
+      }
+    `}</style>
+    <style jsx global>{`
+      html {
+        background-color: #333;
+      }
+      body {
+        font-family: system-ui, -apple-system, BlinkMacSystemFont, "Helvetica Neue", Helvetica, Arial, sans-serif;
+        line-height: 1.15em;
+        color: #333;
+      }
+      h1,h2,h3,h4,h5,h6 {
+        font-weight: 500;
+        line-height: 1.2;
+      }
       a {
         color: #000;
         font-weight: bold;
@@ -61,12 +80,6 @@ const Layout = (props) => (
       a:hover {
         color: #000;
         text-decoration: underline;
-      }
-      footer {
-        font-size: 13px;
-        color: #777;
-        padding: 40px 0;
-        text-align: center;
       }
     `}</style>
   </Fragment>
