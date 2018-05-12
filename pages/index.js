a9075cac800b99f90f5ba2d21d980edb4ef25a0c
Sujay <me@isujay.com>
2018-05-11 22:21:52 -0500

    update index page

--- pages/index.js
+++ pages/index.js
@@ -1,41 +1,46 @@
 import React, { Component } from 'react';
-// import { get } from '../app/fetch';
+
 import Link from 'next/link';
 
-const Index = (props) => (
-  <ul>
-    {props.data.map((item) =>
-    <li key={item.id}>
-      <Link as={`/item/${item.id}`} href={`/item?id=${item.id}`}>
-        <a>{item.title}</a>
-      </Link>
-    </li>
-    )}
-    <style jsx>{`
-      li {
-        list-style-type: none
-      }
-      a {
-        text-decoration: none;
-        color: #000;
-      }
-    `}
-    </style>
-    <style jsx global>{`
-      body {
-        font-family: Helvetica, sans-serif
-      }
-    `}
-    </style>
-  </ul>
-)
+import { get } from "../components/fetch";
+import Layout from '../components/layout';
 
-Index.getInitialProps = async function() {
-  const res = await fetch('https://api.hackerwebapp.com/news');
-  const data = await res.json();
-  return {
-    data: data
+export default class Index extends Component {
+  static async getInitialProps({ query: { type = "news", page = "1" } }) {
+    const json = await get({ type, page });
+    return { data: json };
+  }
+  render() {
+    const { data } = this.props;
+    return (
+      <Layout>
+        <ul className="list-group">
+          {data.map((item) =>
+          <li key={item.id} className="list-group-item">
+            <Link href={`/item?id=${item.id}`}>
+              <a>{item.title}</a>
+            </Link>
+            <div className="meta">
+              {item.user} posted {item.time_ago}<br/>
+              {item.comments_count} comments
+            </div>
+          </li>
+          )}
+        </ul>
+        <style jsx>{`
+          ul {
+            margin: 0;
+            padding: 0;
+          }
+          li {
+            list-style-type: none;
+          }
+          .meta {
+            font-size: 13px;
+            color: #555;
+          }
+        `}</style>
+      </Layout>
+    )
   }
 }
-
-export default Index;
