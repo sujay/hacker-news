fadbb06695c78e84caee2d709dce3eca6012717f
Sujay <me@isujay.com>
2018-05-13 11:48:43 -0500

    header displays page number

--- components/header.js
+++ components/header.js
@@ -2,9 +2,16 @@ import React, { Component, Fragment } from 'react';
 
 class Header extends Component {
   render() {
+    const { children, page } = this.props;
     return (
       <Fragment>
-        <h2>{this.props.children}</h2>
+        <h2>
+          {children}
+          {page && page > 1 &&
+            <span>Page {page}</span>
+          }
+        </h2>
+
         <style jsx>{`
           h2 {
             background-color: #DDD;
@@ -13,6 +20,17 @@ class Header extends Component {
             margin: 0;
             padding: 20px;
             font-weight: bold;
+          }
+          h2 span {
+            float: right;
+            color: #888;
+            height: 100%;
+            vertical-align: center;
+            display: block;
+            font-size: 14px;
+            font-weight: normal;
+            position: relative;
+            top: 7px;
           }
         `}</style>
       </Fragment>
