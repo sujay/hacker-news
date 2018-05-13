33db57bafd56f52f385f274855994537fc6d793f
Sujay <me@isujay.com>
2018-05-12 21:44:40 -0500

    comments are now nested

--- components/comment-detail.js
+++ components/comment-detail.js
@@ -1,6 +1,8 @@
 import React, { Component, Fragment } from 'react';
 import Link from 'next/link';
 
+import Comment from '../components/comment';
+
 class CommentDetail extends Component {
   render() {
     const comments = this.props.data.comments;
@@ -9,18 +11,7 @@ class CommentDetail extends Component {
         {comments.length > 0 &&
         <div className="comments">
           <h5>Comments:</h5>
-          {comments.map((comment) =>
-          <div className="comment" key={comment.id}>
-            <div className="meta">
-              <span className="user">
-                <Link href={{ pathname: 'user', query: {name: comment.user} }}>
-                  <a>{comment.user}</a>
-                </Link>
-              </span>
-              <span> said </span> <span className="time" title={comment.time}>{comment.time_ago}</span>:</div>
-            <div className="content" dangerouslySetInnerHTML={{ __html: comment.content }}></div>
-          </div>
-          )}
+          <Comment comments={comments} />
         </div>
         }
         <style jsx>{`
@@ -28,36 +19,14 @@ class CommentDetail extends Component {
             background-color: #eee;
             color: #111;
             padding: 15px 20px;
-            margin-top: 0px;
-            margin-bottom: 5px;
+            margin-top: -20px;
+            margin-bottom: 20px;
+            margin-left: -20px;
+            margin-right: -20px;
             font-size: 16px;
           }
-          .comment {
-            font-size: 13px;
-            padding: 10px;
-            border-bottom: solid 1px #eee;
-            padding-bottom: 0;
-          }
-          .comment .meta {
-            background-color: #eee;
-            padding: 5px 10px;
-            font-size: 13px;
-            color: #666;
-            border-radius: 3px;
-          }
-          .comment .user {
-            color: #000;;
-          }
-          .comment .user a {
-            font-weight: normal;
-          }
-          .comment .content {
-            line-height: 1.3em;
-            padding: 10px;
-            padding-top: 0;
-            padding-bottom: 0;
-            word-wrap: break-word;
-            overflow: scroll;
+          .comments {
+            padding: 20px;
           }
         `}</style>
       </Fragment>
---
+++ components/comment.js
@@ -0,0 +1,64 @@
+import React, { Component, Fragment } from 'react';
+import Link from 'next/link';
+
+class Comment extends Component {
+  render() {
+    const { comments } = this.props;
+    return (
+      <Fragment>
+        {comments.map((comment) =>
+        <div className="comment" key={comment.id}>
+          <div className="meta">
+            <span className="user">
+              <Link href={{ pathname: 'user', query: {name: comment.user} }}>
+                <a>{comment.user}</a>
+              </Link>
+            </span>
+            <span> said </span> <span className="time" title={comment.time}>{comment.time_ago}</span>:</div>
+          <div className="content" dangerouslySetInnerHTML={{ __html: comment.content }}></div>
+          <Comment comments={comment.comments} />
+        </div>
+        )}
+        <style jsx>{`
+          .comment {
+            font-size: 13px;
+            padding: 0px;
+            padding-bottom: 0;
+            padding-right: 0;
+            border-left: solid 1px #eee;
+            border-top-left-radius: 5px;
+            overflow: hidden;
+            margin-bottom: -3px;
+          }
+          .comment .comment {
+            margin-left: 20px;
+          }
+          .comment .meta {
+            background-color: #eee;
+            padding: 15px 20px;
+            font-size: 13px;
+            color: #666;
+          }
+          .comment .user {
+            color: #000;;
+          }
+          .comment .user a {
+            font-weight: normal;
+          }
+          .comment .content {
+            line-height: 1.3em;
+            padding: 20px;
+            margin-bottom: 8px;
+            word-wrap: break-word;
+            overflow: scroll;
+          }
+          pre {
+            font-size: 11px;
+          }
+        `}</style>
+      </Fragment>
+    )
+  }
+}
+
+export default Comment;
