a27692d63579cd30e50c0b3b87f60fd25758ca8e
Sujay <me@isujay.com>
2018-05-13 15:06:52 -0500

    custom list and styling for jobs page

--- pages/jobs.js
+++ pages/jobs.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Link from 'next/link';
 
 import { get } from "../components/fetch";
 import Layout from '../components/layout';
@@ -15,7 +16,27 @@ export default class Jobs extends Component {
     return (
       <Layout>
         <Header>Jobs</Header>
-        <ListDetail data={data} />
+        <ul>
+          {data.map((item) =>
+          <li key={item.id}>
+            <h6>
+              <Link href={item.url}><a rel="nofollow">{item.title}</a></Link>
+            </h6>
+          </li>
+          )}
+        </ul>
+        <style jsx>{`
+          li {
+            padding: 20px;
+            list-style-type: none;
+            border-bottom: solid 1px #eee;
+          }
+          h6 {
+            font-size: 15px;
+            margin: 0;
+            margin-bottom: 2px;
+          }
+        `}</style>
       </Layout>
     )
   }
