0320cc274a812d5af04345ead8f4f9f25c7e7c59
Sujay <me@isujay.com>
2018-05-12 22:05:44 -0500

    new pagination styling

--- components/page-nav.js
+++ components/page-nav.js
@@ -10,18 +10,51 @@ class PageNav extends Component {
     return (
       <Fragment>
         <div className="pagination">
-          <span className="left">{page > 1 && <Link href={{pathname:pathname, query:{page: previous}}}><a>&larr; Previous</a></Link>}</span>
+          <span className="left">{page > 1 && <Link href={{pathname:pathname, query:{page: previous}}}><a>&larr; </a></Link>}</span>
           <span className="current">Page {page}</span>
-          <span className="right">{page < 10 && <Link href={{pathname:pathname, query:{page: next}}}><a>Next &rarr;</a></Link>}</span>
+          <span className="right">{page < 10 && <Link href={{pathname:pathname, query:{page: next}}}><a> &rarr;</a></Link>}</span>
         </div>
         <style jsx>{`
           .pagination {
             text-align: center;
             padding: 30px;
+            background-color: #eee;
+          }
+          .pagination {
+            display: flex;
+            flex-direction: row;
+            flex-wrap: nowrap;
+            justify-content: center;
+            align-content: stretch;
+            align-items: flex-start;
           }
           span {
-            display: inline-block;
-            margin: 0 10px;
+            width: 33%;
+          }
+          span a:hover {
+            color: #F96630;
+            text-decoration: none;
+          }
+          .left, .right {
+            font-size: 20px;
+          }
+          .left {
+            text-align: right;
+            order: 0;
+            flex: 1 0 auto;
+            align-self: auto;
+          }
+          .current {
+            width: 20%;
+            order: 0;
+            flex: 0 1 auto;
+            align-self: auto;
+          }
+          .right {
+            text-align: left;
+            order: 0;
+            flex: 1 0 auto;
+            align-self: auto;
           }
         `}</style>
       </Fragment>
