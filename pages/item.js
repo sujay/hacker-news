b59f7cf328dd7811083ebca978561f1d74a76258
Sujay <me@isujay.com>
2018-09-17 21:07:23 -0500

    space in title for item pages

--- pages/item.js
+++ pages/item.js
@@ -19,6 +19,7 @@ export default class Item extends Component {
         <Head>
           <title>
             Hacker News -
+            {' '}
             {data.title}
           </title>
         </Head>
