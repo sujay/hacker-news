ff3acc0aaa32c148cf818862817cd875d95e2abf
Sujay <me@isujay.com>
2021-02-22 17:20:00 -0600

    don't make fetch function request anything without arguments

--- helpers/fetch.ts
+++ helpers/fetch.ts
@@ -2,22 +2,31 @@ const endpointPrefix = 'https://hacker-news.firebaseio.com/v0/';
 const endpointSuffix = '.json';
 
 export const getList = async (list: string) => {
-  const endpoint = `${endpointPrefix}${list}${endpointSuffix}`;
-  const res = await fetch(endpoint);
-  const json = await res.json();
-  return json;
+  if(list) {
+    const endpoint = `${endpointPrefix}${list}${endpointSuffix}`;
+    const res = await fetch(endpoint);
+    const json = await res.json();
+    return json;
+  }
+  return null;
 };
 
 export const getItem = async (item: number) => {
-  const endpoint = `${endpointPrefix}item/${item}${endpointSuffix}`;
-  const res = await fetch(endpoint);
-  const json = await res.json();
-  return json;
+  if(item) {
+    const endpoint = `${endpointPrefix}item/${item}${endpointSuffix}`;
+    const res = await fetch(endpoint);
+    const json = await res.json();
+    return json;
+  }
+  return null;
 };
 
 export const getUser = async (user: string) => {
-  const endpoint = `${endpointPrefix}user/${user}${endpointSuffix}`;
-  const res = await fetch(endpoint);
-  const json = await res.json();
-  return json;
+  if(user) {
+    const endpoint = `${endpointPrefix}user/${user}${endpointSuffix}`;
+    const res = await fetch(endpoint);
+    const json = await res.json();
+    return json;
+  }
+  return null;
 };
