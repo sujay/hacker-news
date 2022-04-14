e735f9c1505ec2da4a015fcc83eaf4d00fc757ac
Sujay <me@isujay.com>
2022-04-13 22:20:34 -0500

    simplify fetch

--- helpers/fetch.ts
+++ helpers/fetch.ts
@@ -1,32 +1,17 @@
 const endpointPrefix = 'https://hacker-news.firebaseio.com/v0/';
 const endpointSuffix = '.json';
 
-export const getList = async (list: string) => {
-  if (list) {
-    const endpoint = `${endpointPrefix}${list}${endpointSuffix}`;
-    const res = await fetch(endpoint);
-    const json = await res.json();
-    return json;
-  }
-  return null;
-};
+export const getList = (list: string) =>
+  fetch(`${endpointPrefix}${list}${endpointSuffix}`).then((response) =>
+    response.json(),
+  );
 
-export const getItem = async (item: number) => {
-  if (item) {
-    const endpoint = `${endpointPrefix}item/${item}${endpointSuffix}`;
-    const res = await fetch(endpoint);
-    const json = await res.json();
-    return json;
-  }
-  return null;
-};
+export const getItem = (itemId: number) =>
+  fetch(`${endpointPrefix}item/${itemId}${endpointSuffix}`).then((response) =>
+    response.json(),
+  );
 
-export const getUser = async (user: string) => {
-  if (user) {
-    const endpoint = `${endpointPrefix}user/${user}${endpointSuffix}`;
-    const res = await fetch(endpoint);
-    const json = await res.json();
-    return json;
-  }
-  return null;
-};
+export const getUser = (user: string) =>
+  fetch(`${endpointPrefix}user/${user}${endpointSuffix}`).then((response) =>
+    response.json(),
+  );
