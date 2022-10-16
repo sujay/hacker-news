a9b953ff36a7134181165508234721f93ac4d6f1
Sujay <me@isujay.com>
2022-10-15 19:14:40 -0500

    fetch error on non-2xx http status codes

--- helpers/fetch.ts
+++ helpers/fetch.ts
@@ -2,16 +2,24 @@ const endpointPrefix = 'https://hacker-news.firebaseio.com/v0/';
 const endpointSuffix = '.json';
 
 export const getList = (list: string) =>
-  fetch(`${endpointPrefix}${list}${endpointSuffix}`).then((response) =>
-    response.json(),
-  );
-
+  fetch(`${endpointPrefix}${list}${endpointSuffix}`).then((response) => {
+    if (!response.ok) {
+      throw new Error(`Status code error: ${response.status}`);
+    }
+    return response.json();
+  });
 export const getItem = (itemId: number) =>
-  fetch(`${endpointPrefix}item/${itemId}${endpointSuffix}`).then((response) =>
-    response.json(),
-  );
+  fetch(`${endpointPrefix}item/${itemId}${endpointSuffix}`).then((response) => {
+    if (!response.ok) {
+      throw new Error(`Status code error: ${response.status}`);
+    }
+    return response.json();
+  });
 
 export const getUser = (user: string) =>
-  fetch(`${endpointPrefix}user/${user}${endpointSuffix}`).then((response) =>
-    response.json(),
-  );
+  fetch(`${endpointPrefix}user/${user}${endpointSuffix}`).then((response) => {
+    if (!response.ok) {
+      throw new Error(`Status code error: ${response.status}`);
+    }
+    return response.json();
+  });
