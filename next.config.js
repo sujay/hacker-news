f1c62df015925fd1a6580c34e3d1da5a0badd588
Sujay <me@isujay.com>
2023-04-14 12:08:38 -0500

    config updates

--- next.config.js
+++ next.config.js
@@ -1,25 +1,7 @@
 module.exports = {
   experimental: {
     scrollRestoration: true,
+    appDir: true,
   },
   poweredByHeader: false,
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.(png)$/,
-      use: [
-        {
-          loader: 'file-loader',
-          options: {
-            publicPath: '/_next/static/hn',
-            outputPath: 'static/hn',
-            name: '[name]-[hash].[ext]',
-          },
-        },
-      ],
-    });
-    return config;
-  },
-  images: {
-    disableStaticImages: true,
-  },
 };
