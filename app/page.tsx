e32c67bd88ea775269f66895f719203516bdbcab
Sujay <me@isujay.com>
2023-04-14 20:15:52 -0500

    metadata?

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import { Metadata } from 'next';
 
 import { getList } from '../helpers/fetch';
 import ListDetail from '../components/list-detail';
+
+export const metadata: Metadata = {
+  title: 'Hacker News',
+  description:
+    'A Hacker News clone built with React and Next.js. Hacker News is a social news website focusing on computer science and entrepreneurship.',
+};
 
 export default async function Index() {
   const getListData = getList('topstories');
