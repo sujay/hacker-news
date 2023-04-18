f25f4fab856c66d1f553f03b14d4e03dbd396064
Sujay <me@isujay.com>
2023-04-17 21:47:10 -0500

    new analytics code

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,28 @@
 import React from 'react';
 import Link from 'next/link';
+import Script from 'next/script';
 
 import './global.css';
 import styles from './layout.module.css';
 
 import Nav from '../components/nav';
 
+if (!process.env.NEXT_PUBLIC_GTM_ID) {
+  process.env.NEXT_PUBLIC_GTM_ID = '';
+}
+
 export default function Layout({ children }: React.PropsWithChildren) {
   return (
     <html lang="en">
-      <head>
-        <title>Hacker News</title>
-      </head>
+      <Script id="gtag" strategy="afterInteractive">
+        {`
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GTM_ID}');
+      `}
+      </Script>
       <body>
         <div id={styles.container}>
           <header className={styles.header}>
@@ -66,6 +77,15 @@ export default function Layout({ children }: React.PropsWithChildren) {
             </div>
           </footer>
         </div>
+        <noscript>
+          <iframe
+            title="NOSCRIPTGTAG"
+            src="https://www.googletagmanager.com/ns.html?id=GTM-K3NZVMD"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
       </body>
     </html>
   );
