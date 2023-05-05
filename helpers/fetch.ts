63cb1e218540db4923adc8d54e488f64222ab455
Sujay <me@isujay.com>
2023-05-04 21:39:25 -0500

    clean up fetch utility and catch errors

--- helpers/fetch.ts
+++ helpers/fetch.ts
@@ -1,31 +1,33 @@
 const endpointPrefix = 'https://hacker-news.firebaseio.com/v0/';
 const endpointSuffix = '.json';
 
+const fetchData = (endpoint: string, cache: Object) =>
+  fetch(endpoint, cache)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`Status code error: ${response.status}`);
+      }
+      return response.json();
+    })
+    .catch((error) => {
+      if (error instanceof TypeError) {
+        throw new Error(`Failed to fetch data: ${error.message}`);
+      } else {
+        throw new Error(`Something went wrong: ${error.message}`);
+      }
+    });
+
 export const getList = (list: string) =>
-  fetch(`${endpointPrefix}${list}${endpointSuffix}`, {
+  fetchData(`${endpointPrefix}${list}${endpointSuffix}`, {
     next: { revalidate: 60 },
-  }).then((response) => {
-    if (!response.ok) {
-      throw new Error(`Status code error: ${response.status}`);
-    }
-    return response.json();
-  });
-export const getItem = (itemId: number) =>
-  fetch(`${endpointPrefix}item/${itemId}${endpointSuffix}`, {
-    cache: 'no-store',
-  }).then((response) => {
-    if (!response.ok) {
-      throw new Error(`Status code error: ${response.status}`);
-    }
-    return response.json();
   });
 
-export const getUser = (user: string) =>
-  fetch(`${endpointPrefix}user/${user}${endpointSuffix}`, {
+export const getItem = async (itemId: number) =>
+  fetchData(`${endpointPrefix}item/${itemId}${endpointSuffix}`, {
     cache: 'no-store',
-  }).then((response) => {
-    if (!response.ok) {
-      throw new Error(`Status code error: ${response.status}`);
-    }
-    return response.json();
+  });
+
+export const getUser = async (user: string) =>
+  fetchData(`${endpointPrefix}user/${user}${endpointSuffix}`, {
+    cache: 'no-store',
   });
