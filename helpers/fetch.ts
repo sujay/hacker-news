519992fede2994a76530185da3e395380bb7b62d
Sujay <me@isujay.com>
2023-05-21 22:10:50 -0500

    use ky instead of fetch

--- helpers/fetch.ts
+++ helpers/fetch.ts
@@ -1,21 +1,10 @@
+import ky from 'ky';
+
 const endpointPrefix = 'https://hacker-news.firebaseio.com/v0/';
 const endpointSuffix = '.json';
 
 const fetchData = (endpoint: string, cache: Object) =>
-  fetch(endpoint, cache)
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error(`Status code error: ${response.status}`);
-      }
-      return response.json();
-    })
-    .catch((error) => {
-      if (error instanceof TypeError) {
-        throw new Error(`Failed to fetch data: ${error.message}`);
-      } else {
-        throw new Error(`Something went wrong: ${error.message}`);
-      }
-    });
+  ky.get(endpoint, cache).json();
 
 export const getList = (list: string) =>
   fetchData(`${endpointPrefix}${list}${endpointSuffix}`, {
