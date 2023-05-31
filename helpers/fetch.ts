ce5feae72e43ad2d528b0eb0561dc12a2a1fe9a7
Sujay <me@isujay.com>
2023-05-30 22:16:28 -0500

    don't error handle fetch

--- helpers/fetch.ts
+++ helpers/fetch.ts
@@ -2,20 +2,7 @@ const endpointPrefix = 'https://hacker-news.firebaseio.com/v0/';
 const endpointSuffix = '.json';
 
 const fetchData = (endpoint: string, cache: Object) =>
-  fetch(endpoint, cache)
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error(`Status code error: ${response.status}`);
-      }
-      return response.json();
-    })
-    .catch((error) => {
-      if (error instanceof TypeError) {
-        throw new Error(`Failed to fetch data: ${error.message}`);
-      } else {
-        throw new Error(`Something went wrong: ${error.message}`);
-      }
-    });
+  fetch(endpoint, cache).then((response) => response.json());
 
 export const getList = (list: string) =>
   fetchData(`${endpointPrefix}${list}${endpointSuffix}`, {
