8a5517ef761e6f9f2ab9f7ce4f1637446d7649c8
Sujay <me@isujay.com>
2023-06-23 19:43:36 -0500

    re-add header in user page

--- app/user/[name]/page.tsx
+++ app/user/[name]/page.tsx
@@ -6,6 +6,7 @@ import listStyles from '../../../components/list-item.module.css';
 
 import { getUser } from '../../../helpers/fetch';
 
+import Header from '../../../components/header';
 import Time from '../../../components/time';
 
 import { UserProps } from '../../../types/interfaces';
@@ -28,43 +29,46 @@ export default async function User({ params }: { params: { name: string } }) {
   const user = (await getUser(`${params!.name}`)) as UserProps;
 
   return user ? (
-    <div className={styles.user}>
-      <h3 className={styles.h3}>{user.id}</h3>
-      {user.created && (
-        <div className={styles.meta}>
-          <span className={styles.label}>Registered:</span>
-          <span className={styles.content}>
-            <Time time={user.created} />
-          </span>
-        </div>
-      )}
-      {user.karma && (
-        <div className={styles.meta}>
-          <span className={styles.label}>Karma:</span>
-          <span className={styles.content}>{`${user.karma} points`}</span>
-        </div>
-      )}
-      {user.submitted && (
-        <div className={styles.meta}>
-          <span className={styles.label}>Posted:</span>
-          <span
-            className={styles.content}
-          >{`${user.submitted.length} times`}</span>
-        </div>
-      )}
-      {user.about && (
-        <div className={styles.meta}>
-          <span className={styles.label}>About:</span>
-          <span
-            className="content about_text"
-            // eslint-disable-next-line react/no-danger
-            dangerouslySetInnerHTML={{
-              __html: sanitizeHtml(user.about),
-            }}
-          />
-        </div>
-      )}
-    </div>
+    <>
+      <Header>User</Header>
+      <div className={styles.user}>
+        <h3 className={styles.h3}>{user.id}</h3>
+        {user.created && (
+          <div className={styles.meta}>
+            <span className={styles.label}>Registered:</span>
+            <span className={styles.content}>
+              <Time time={user.created} />
+            </span>
+          </div>
+        )}
+        {user.karma && (
+          <div className={styles.meta}>
+            <span className={styles.label}>Karma:</span>
+            <span className={styles.content}>{`${user.karma} points`}</span>
+          </div>
+        )}
+        {user.submitted && (
+          <div className={styles.meta}>
+            <span className={styles.label}>Posted:</span>
+            <span
+              className={styles.content}
+            >{`${user.submitted.length} times`}</span>
+          </div>
+        )}
+        {user.about && (
+          <div className={styles.meta}>
+            <span className={styles.label}>About:</span>
+            <span
+              className="content about_text"
+              // eslint-disable-next-line react/no-danger
+              dangerouslySetInnerHTML={{
+                __html: sanitizeHtml(user.about),
+              }}
+            />
+          </div>
+        )}
+      </div>
+    </>
   ) : (
     <li className={listStyles.li}>Error loading user.</li>
   );
