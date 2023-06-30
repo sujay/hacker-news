5f196df1409e7937e84449ebacb8110395da2e6b
Sujay <me@isujay.com>
2023-06-30 15:47:22 -0500

    remove unused import

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Link from 'next/link';
 import { usePathname, useRouter, useSearchParams } from 'next/navigation';
 
