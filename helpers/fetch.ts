c8bc68fe740ba7cb71b8c9e040da730adf22da0a
Sujay <me@isujay.com>
2024-04-27 18:41:14 -0500

    individual cache times

--- helpers/fetch.ts
+++ helpers/fetch.ts
@@ -1,7 +1,5 @@
-async function fetchData(route: string) {
-  const res = await fetch(route, {
-    next: { revalidate: 3600 },
-  });
+async function fetchData(route: string, cache = {}) {
+  const res = await fetch(route, cache);
   if (!res.ok) {
     throw new Error('Failed to fetch data!');
   }
@@ -9,13 +7,22 @@ async function fetchData(route: string) {
 }
 
 export const getList = async (list: string) =>
-  fetchData(`https://api.hackerwebapp.com/${list}`);
+  fetchData(`https://api.hackerwebapp.com/${list}`, {
+    next: { revalidate: 21600 },
+  });
 
 export const getItem = async (itemId: number) =>
-  fetchData(`https://api.hackerwebapp.com/item/${itemId}`);
+  fetchData(`https://api.hackerwebapp.com/item/${itemId}`, {
+    next: { revalidate: 3600 },
+  });
+
+export const getMeta = async (itemId: number) =>
+  fetchData(`https://hacker-news.firebaseio.com/v0/item/${itemId}.json`);
 
 export const getUser = async (user: string) =>
   fetchData(`https://hacker-news.firebaseio.com/v0/user/${user}.json`);
 
 export const getSearch = async (query: string) =>
-  fetchData(`https://hn.algolia.com/api/v1/search?query=${query}&tags=story`);
+  fetchData(`https://hn.algolia.com/api/v1/search?query=${query}&tags=story`, {
+    next: { revalidate: 86400 },
+  });
