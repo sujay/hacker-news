9ce548da4304e473601616252211c4a7f249ecc9
Sujay <me@isujay.com>
2024-05-02 17:47:28 -0500

    item page components can be combined
    next handles progressive loading of the base page while it fetches
    
    for some reason it wasn't working before

--- app/item/[id]/page.tsx
+++ app/item/[id]/page.tsx
@@ -1,17 +1,56 @@
 import React, { Suspense } from 'react';
 import { notFound } from 'next/navigation';
 
-import ItemRender from './item';
-import LoadingPage from '../../loading';
+import styles from './page.module.css';
+import listStyles from '../../../components/list-item.module.css';
 
-export default function Item({ params }: { params: { id: string } }) {
-  if (Number.isNaN(+params!.id)) {
-    notFound();
+import Header from '../../../components/header';
+import Comments from '../../../components/comments';
+import ItemDetail from '../../../components/item-detail';
+import Loading from '../../../components/loading';
+import { getMeta } from '../../../helpers/fetch';
+
+export default async function ItemRender({
+  params,
+}: {
+  params: { id: string };
+}) {
+  const item = await getMeta(+params.id);
+
+  if (Number.isNaN(+params!.id) || !item) {
+    return notFound();
   }
 
+  const title = `${
+    item.title || `${item.type.charAt(0).toUpperCase() + item.type.slice(1)}`
+  } - Hacker News`;
+
   return (
-    <Suspense fallback={<LoadingPage />}>
-      <ItemRender id={params.id} />
-    </Suspense>
+    <>
+      <title>{title}</title>
+      <meta name="robots" content="none" />
+      <Header>{item.type === 'link' ? 'story' : item.type}</Header>
+      {!item.dead && !item.deleted ? (
+        <>
+          <ItemDetail item={item} page="item" />
+          {item.descendants > 0 && (
+            <div className={styles.comments}>
+              <h4 className={styles.h4}>
+                {item.descendants}
+                {item.descendants > 1 ? ' Comments:' : ' Comment:'}
+              </h4>
+              <Suspense fallback={<Loading />}>
+                <Comments id={item.id} />
+              </Suspense>
+            </div>
+          )}
+        </>
+      ) : (
+        <li className={listStyles.li}>
+          {item.type[0].toUpperCase() + item.type.substring(1)} has been
+          deleted.
+        </li>
+      )}
+    </>
   );
 }
