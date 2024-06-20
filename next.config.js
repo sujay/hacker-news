6167e5c928248b5a3e28e014da5077214f5c2481
Sujay <me@isujay.com>
2024-06-20 17:02:03 -0500

    disable sentry telemetry

--- next.config.js
+++ next.config.js
@@ -9,6 +9,8 @@ module.exports = withSentryConfig(nextConfig, {
   // For all available options, see:
   // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
 
+  telemetry: false,
+
   // Upload a larger set of source maps for prettier stack traces (increases build time)
   widenClientFileUpload: true,
 
